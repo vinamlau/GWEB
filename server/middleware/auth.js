57749a5c922b8733ea934b29a4023765026d1edf
vinamlau <victor.bizchina@gmail.com>
2026-04-05 15:35:04 +0800

    fix: 修复 auth.js 中间件的 db 导入问题
    - 将 auth.js 中的 db 导入改为 const { db } = require(...)
    - 修复认证中间件无法访问 db.prepare 的问题

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const db = require('../config/db-sqlite')
+const { db } = require('../config/db-sqlite')
 
 const protect = async (req, res, next) => {
   let token
